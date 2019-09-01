4c1352fffe214029503e181ce0a8a730a49ee3c1
jakub41 <lemiszewski@gmx.com>
2019-09-01 15:46:25 +0200

    Fix update card

--- src/graphql/mutations/update-card.ts
+++ src/graphql/mutations/update-card.ts
@@ -5,7 +5,7 @@ export const updateCardMutation = {
     async updateCard(_, { id, patch }): Promise<Card> {
         const repository = getRepository(Card);
         const card = await repository.findOne({ id });
-        const result = await repository.update(id, patch);
+        const result = await repository.update(id, { ...patch });
         return {
             ...card,
             ...patch,
