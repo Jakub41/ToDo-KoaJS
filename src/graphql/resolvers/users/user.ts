4a6d6aada28da84dc0e5660a507e3c48da35b0ab
jakub41 <lemiszewski@gmx.com>
2019-09-09 13:50:21 +0200

    Fixed wrong value

--- src/graphql/resolvers/users/user.ts
+++ src/graphql/resolvers/users/user.ts
@@ -2,7 +2,7 @@ import { getRepository } from 'typeorm';
 import { Entities } from '../../../entities/entities';
 
 export const userResolver = {
-    async card(obj, { id }, context, info): Promise<typeof userResolver> {
+    async user(obj, { id }, context, info): Promise<typeof userResolver> {
         const repository = getRepository(Entities.user);
         return await repository.findOne({ id });
     },
