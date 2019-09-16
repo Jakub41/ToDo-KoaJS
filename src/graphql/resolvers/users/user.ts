e0a61c8482d94e1b4af947a535974e8850a8de4c
jakub41 <lemiszewski@gmx.com>
2019-09-16 15:22:47 +0200

    Fixed wrong name method

--- src/graphql/resolvers/users/user.ts
+++ src/graphql/resolvers/users/user.ts
@@ -2,7 +2,7 @@ import { getRepository } from 'typeorm';
 import { Entities } from '../../../entities/entities';
 
 export const userResolver = {
-    async card(_, { id }): Promise<typeof userResolver> {
+    async user(_, { id }): Promise<typeof userResolver> {
         const repository = getRepository(Entities.user);
         return await repository.findOne(id);
     },
