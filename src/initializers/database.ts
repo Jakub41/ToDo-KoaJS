2863b20d8e28a6dd400332bc6ba7108c9b9922b9
jakub41 <lemiszewski@gmx.com>
2019-09-20 00:10:00 +0200

    removed no need elements

--- src/initializers/database.ts
+++ src/initializers/database.ts
@@ -17,18 +17,6 @@ export const databaseInitializer = async (): Promise<void> => {
     })
         .then(async () => {
             console.log(`Database connection established on port: ${config.dbPort}`);
-
-            /** To Create Some Fake Data Example **/
-
-            // Create new instance
-            // const card = new Card();
-            // card.title = 'First card';
-            // card.done  = false;
-
-            // Persist to database
-            // const card_1 = await connection.manager
-            //       .save(card);
-            //   console.log('card saved');
         })
         .catch(error => console.log('Connection error: ', error));
 };
