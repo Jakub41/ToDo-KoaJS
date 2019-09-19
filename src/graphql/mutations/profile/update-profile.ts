20bee3154c170e7552a46730bbb0ff98bc19d8a6
jakub41 <lemiszewski@gmx.com>
2019-09-19 22:30:54 +0200

    removed const result as was no need in UserUpdate and ProfileUpdate

--- src/graphql/mutations/profile/update-profile.ts
+++ src/graphql/mutations/profile/update-profile.ts
@@ -5,7 +5,7 @@ export const updateProfileMutation = {
     async updateProfile(_, { id, patch }): Promise<typeof profile> {
         const repository = getRepository(Entities.profile);
         const profile = await repository.findOne({ id });
-        const result = await repository.update(id, { ...patch });
+        repository.update(id, { ...patch });
         return {
             ...profile,
             ...patch,
--- src/graphql/mutations/user/update-user.ts
+++ src/graphql/mutations/user/update-user.ts
@@ -5,7 +5,7 @@ export const updateUserMutation = {
     async updateUser(_, { id, patch }): Promise<typeof user> {
         const repository = getRepository(Entities.user);
         const user = await repository.findOne({ id });
-        const result = await repository.update(id, { ...patch });
+        repository.update(id, { ...patch });
         return {
             ...user,
             ...patch,
